f5f176990d36d8cfeb514c07d45607b1b587cc54
antondc <hello@antoniodiaz.me>
2022-01-02 23:29:23 +0300

    Texteditor basic formatting

--- src/shared/components/TextEditor/index.tsx
+++ src/shared/components/TextEditor/index.tsx
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
-import { createEditor, Descendant } from 'slate';
+import React, { useCallback, useState } from 'react';
+import { createEditor, Descendant, Text, Transforms } from 'slate';
 import { Editable, Slate, withReact } from 'slate-react';
 
 import './TextEditor.less';
+
+// Define a React component renderer for our code blocks.
+const CodeElement = (props) => (
+  <pre {...props.attributes}>
+    <code>{props.children}</code>
+  </pre>
+);
+
+const DefaultElement = (props) => <p {...props.attributes}>{props.children}</p>;
 
 interface Props {
   value: string;
@@ -14,10 +23,77 @@ const TextEditor: React.FC<Props> = ({ value }) => {
   const [editor] = useState(() => withReact(createEditor()));
   const [localValue, setLocalValue] = useState<Descendant[]>(parsedValue);
 
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
+  const Leaf = (props) => (
+    <span
+      {...props.attributes}
+      className={
+        'TextEditor-span' +
+        (props.leaf.bold ? ' TextEditor-span--bold' : '') +
+        (props.leaf.italic ? ' TextEditor-span--italic' : '')
+      }
+    >
+      {props.children}
+    </span>
+  );
+
+  // Define a leaf rendering function that is memoized with `useCallback`.
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+
   return (
-    <Slate editor={editor} value={localValue} onChange={setLocalValue}>
-      <Editable />
-    </Slate>
+    <div className="TextEditor">
+      <Slate editor={editor} value={localValue} onChange={setLocalValue}>
+        <Editable
+          renderElement={renderElement}
+          renderLeaf={renderLeaf}
+          onKeyDown={(event) => {
+            if (!event.ctrlKey) {
+              return;
+            }
+
+            switch (event.key) {
+              case 'b': {
+                event.preventDefault();
+                Transforms.setNodes(
+                  editor,
+                  {
+                    bold: true,
+                  },
+                  {
+                    match: (node: Text) => Text.isText(node),
+                    split: true,
+                  }
+                );
+                break;
+              }
+
+              case 'i': {
+                event.preventDefault();
+                Transforms.setNodes(
+                  editor,
+                  {
+                    italic: true,
+                  },
+                  {
+                    match: (node: Text) => Text.isText(node),
+                    split: true,
+                  }
+                );
+                break;
+              }
+            }
+          }}
+        />
+      </Slate>
+    </div>
   );
 };
 
--- src/shared/components/TextEditor/types.ts
+++ src/shared/components/TextEditor/types.ts
@@ -1,19 +1,29 @@
-import { BaseEditor } from 'slate';
+import { BaseEditor, Node } from 'slate';
 import { ReactEditor } from 'slate-react';
 
+export interface CustomText {
+  type: string;
+  text: string;
+  bold?: boolean;
+  code?: boolean;
+  italic?: boolean;
+  underlined?: boolean;
+}
+
 export type CustomElement = {
-  type: 'paragraph';
+  type: 'paragraph' | 'code';
   children: CustomText[];
 };
 
-export type CustomText = {
-  text: string;
+export type CustomNode = Node & {
+  bold: boolean;
 };
 
 declare module 'slate' {
   interface CustomTypes {
-    Editor: BaseEditor & ReactEditor;
+    Editor: BaseEditor & ReactEditor & { type: string };
     Element: CustomElement;
     Text: CustomText;
+    Node: CustomNode;
   }
 }
