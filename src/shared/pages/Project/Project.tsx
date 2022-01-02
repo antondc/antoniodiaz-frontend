f294fb744a83b9ec550f0eba02766a2cacd2b728
antondc <hello@antoniodiaz.me>
2022-01-02 16:01:30 +0300

    Render slider if items

--- src/shared/pages/Project/Project.tsx
+++ src/shared/pages/Project/Project.tsx
@@ -4,6 +4,7 @@ import ReactHtmlParser from 'react-html-parser';
 import BaseCarousel from 'Components/BaseCarousel';
 import Img from 'Components/Img';
 import { ProjectState } from 'Modules/Projects/projects.types';
+import { slides } from './constants';
 
 import './Project.less';
 
@@ -24,20 +25,22 @@ interface Props {
 export const Project: React.FC<Props> = ({ lang, project, slidesWithData }) => (
   <div className="Project">
     <div className="Project-title">{project.translations[lang]?.title}</div>
-    <BaseCarousel>
-      {slidesWithData?.map((item) => (
-        <div className="Project-slide" key={item.src}>
-          <Img
-            className="Project-slideImage"
-            src={item.src}
-            sizes={item.sizes}
-            srcSet={item.srcSet}
-            title={item.title}
-            alt={item.title}
-          />
-        </div>
-      ))}
-    </BaseCarousel>
+    {!!slides.length && (
+      <BaseCarousel>
+        {slidesWithData?.map((item) => (
+          <div className="Project-slide" key={item.src}>
+            <Img
+              className="Project-slideImage"
+              src={item.src}
+              sizes={item.sizes}
+              srcSet={item.srcSet}
+              title={item.title}
+              alt={item.title}
+            />
+          </div>
+        ))}
+      </BaseCarousel>
+    )}
     <div className="Project-subtitle">{project.translations[lang]?.subtitle}</div>
     <div className="Project-content">{ReactHtmlParser(project.translations[lang]?.htmlContent)}</div>
   </div>
