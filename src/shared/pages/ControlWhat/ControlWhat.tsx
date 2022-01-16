5015bf28dd597f0f43bd8c0a9f0f9d9921f32c6c
antondc <hello@antoniodiaz.me>
2022-01-16 19:53:06 +0300

    ControlWhat page icons

--- src/shared/pages/ControlWhat/ControlWhat.tsx
+++ src/shared/pages/ControlWhat/ControlWhat.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Edit from 'Assets/svg/edit-2.svg';
+import Move from 'Assets/svg/move-2.svg';
 import A from 'Components/A';
+import { SortableItem, SortableList } from 'Components/SortableList';
 import { GlossaryState } from 'Modules/Languages/languages.types';
 import { ProjectState } from 'Modules/Projects/projects.types';
-import { Button, Fade, SortableItem, SortableList } from '@antoniodcorrea/components';
+import { Button, Fade, Img } from '@antoniodcorrea/components';
 
 import './ControlWhat.less';
 
@@ -29,18 +32,33 @@ export const ControlWhat: React.FC<Props> = ({
         id="ControlWhat-sortable"
         className="ControlWhat-sortable"
         onSortChange={onSortChange}
-        handleClass="ControlWhat-sortableItemHandle"
-        ghostClass="ControlWhat-ghost"
+        handleClass="ControlWhat-sortableHandle"
       >
         {projects?.map((item) => (
-          <li className="ControlWhat-sortableItem" key={item.id} data-id={item.id} data-order={item.order}>
-            <span className="ControlWhat-sortableItemLeft">
-              <A className="ControlWhat-sortableItemTitle" href={`/control/what/${item.id}`} underlined>
-                {item.title}
+          <li className="ControlWhat-sortableItem" data-id={item.id} data-order={item.order} key={item.order}>
+            <Img
+              className="ControlWhat-sortableImage"
+              title={item.title}
+              alt={item.title}
+              src={item.carousel[0].images['original']}
+              sizes="600px"
+              srcSet={
+                item.carousel[0].images['w200'] +
+                ' 200w, ' +
+                item.carousel[0].images['w400'] +
+                ' 400w, ' +
+                item.carousel[0].images['w1200'] +
+                ' 1200w, ' +
+                item.carousel[0].images['w2400'] +
+                ' 2400w, '
+              }
+            />
+            <div className="ControlWhat-sortableIcons">
+              <A href={`/what/${item?.id}`}>
+                <Edit className="ControlWhat-sortableIcon ControlWhat-sortableEdit" />
               </A>
-              <div className="ControlWhat-sortableItemDate">{item.date}</div>
-            </span>
-            <span className="ControlWhat-sortableItemHandle">≡</span>
+              <Move className="ControlWhat-sortableIcon ControlWhat-sortableHandle" />
+            </div>
           </li>
         ))}
       </SortableList>
