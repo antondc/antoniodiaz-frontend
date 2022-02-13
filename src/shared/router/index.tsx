1dd4df8739847e067fa6e7efddd71f1bd611124a
antondc <hello@antoniodiaz.me>
2022-02-14 02:13:10 +0300

    Router fade

--- src/shared/router/index.tsx
+++ src/shared/router/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
+import { selectCurrentRoute } from 'Modules/Routes/selectors/selectCurrentRoute';
 import { selectPathWithoutLanguageParam } from 'Modules/Routes/selectors/selectPathWithoutLanguageParam';
 import { pathsByLayout, RouteLayout } from 'Router/routes';
 import { Location } from 'Services/History';
+import { FadeInOut } from '@antoniodcorrea/components';
 import FullPage from './FullPage';
 
 interface Props {
@@ -13,12 +15,16 @@ interface Props {
 
 const Router: React.FC<Props> = ({ location }) => {
   const pathsByLayoutFullPage = pathsByLayout(RouteLayout.fullPage);
+  const currentRoute = useSelector(selectCurrentRoute);
+  const currentLayout = currentRoute?.layout;
   const pathWithoutLanguageParam = useSelector(selectPathWithoutLanguageParam);
 
   return (
-    <Switch location={{ ...location, pathname: pathWithoutLanguageParam }}>
-      <Route path={pathsByLayoutFullPage} component={FullPage} />
-    </Switch>
+    <FadeInOut valueToUpdate={currentLayout} speed="fastest" appear>
+      <Switch location={{ ...location, pathname: pathWithoutLanguageParam }}>
+        <Route path={pathsByLayoutFullPage} component={FullPage} />
+      </Switch>
+    </FadeInOut>
   );
 };
 
