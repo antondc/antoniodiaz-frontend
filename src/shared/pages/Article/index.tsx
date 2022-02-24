1edb2132c1045f579400b74d8d1a8fab8a3696c8
antondc <hello@antoniodiaz.me>
2022-02-25 01:58:35 +0300

    Article remove unnecesary elements

--- src/shared/pages/Article/index.tsx
+++ src/shared/pages/Article/index.tsx
@@ -8,20 +8,14 @@ import { useMathFormat } from 'Hooks/useMathFormat';
 import { articlesLoad } from 'Modules/Articles/actions/articlesLoad';
 import { ArticleState } from 'Modules/Articles/articles.types';
 import { selectArticle } from 'Modules/Articles/selectors/selectArticle';
-import { selectCurrentGlossary } from 'Modules/Languages/selectors/selectCurrentGlossary';
 import { RootState } from 'Modules/rootType';
 import { selectCurrentRouteParams } from 'Modules/Routes/selectors/selectCurrentRouteParams';
-import { LocaleFormattedDate } from '@antoniodcorrea/utils';
-import { Article as ArticleUi } from './Article';
 
 const Article: React.FC = () => {
   const dispatch = useDispatch();
   const params = useSelector(selectCurrentRouteParams);
   const article = useSelector((state: RootState) => selectArticle(state, Number(params.articleId)));
   const cachedArticle = useCachedData<ArticleState>(article);
-  const date = new LocaleFormattedDate({ unixTime: Number(cachedArticle?.createdAt), locale: params?.lang });
-  const createdAtFormatted = date.getLocaleFormattedDate();
-  const glossary = useSelector(selectCurrentGlossary);
 
   const loadInitialData = async () => {
     await dispatch(articlesLoad());
@@ -32,6 +26,6 @@ const Article: React.FC = () => {
 
   useHljs({ data: article });
 
-  return <ArticleUi article={cachedArticle} date={createdAtFormatted} glossary={glossary} />;
+  return <div>ARTICLE UI</div>;
 };
 export default Article;
