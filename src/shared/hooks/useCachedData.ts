e9f6ec32a1dec971c1fe866bc5c84620bd810e0c
antondc <hello@antoniodiaz.me>
2022-04-14 14:05:11 +0200

    Refactor useCachedData

--- src/shared/hooks/useCachedData.ts
+++ src/shared/hooks/useCachedData.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 
+import { isDomAvailable } from '@antoniodcorrea/utils';
+
 type UseCachedData = <T>(data: T) => T;
 
 export const useCachedData: UseCachedData = <T>(data) => {
@@ -11,5 +13,10 @@ export const useCachedData: UseCachedData = <T>(data) => {
     setCachedData(data);
   }, [data]);
 
-  return cachedData || data;
+  // Skip on server side, as `cachedData` will be always undefined on first time
+  if (!isDomAvailable) {
+    return data;
+  }
+
+  return cachedData;
 };
