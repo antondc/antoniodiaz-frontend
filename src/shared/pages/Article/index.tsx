c824837b959a7b2d76864b521a9756ceddc16620
antondc <hello@antoniodiaz.me>
2022-09-22 19:18:17 +0200

    Styles

--- src/shared/pages/Article/index.tsx
+++ src/shared/pages/Article/index.tsx
@@ -9,6 +9,7 @@ import { articlesLoad } from 'Modules/Articles/actions/articlesLoad';
 import { ArticleState } from 'Modules/Articles/articles.types';
 import { selectArticle } from 'Modules/Articles/selectors/selectArticle';
 import { selectCurrentGlossary } from 'Modules/Languages/selectors/selectCurrentGlossary';
+import { selectCurrentLanguageSlug } from 'Modules/Languages/selectors/selectCurrentLanguageSlug';
 import { RootState } from 'Modules/rootType';
 import { selectCurrentRouteParams } from 'Modules/Routes/selectors/selectCurrentRouteParams';
 import { LocaleFormattedDate } from '@antoniodcorrea/utils';
@@ -17,9 +18,10 @@ import { Article as ArticleUi } from './Article';
 const Article: React.FC = () => {
   const dispatch = useDispatch();
   const params = useSelector(selectCurrentRouteParams);
+  const currentLanguageSlug = useSelector(selectCurrentLanguageSlug);
   const article = useSelector((state: RootState) => selectArticle(state, Number(params.articleId)));
   const cachedArticle = useCachedData<ArticleState>(article);
-  const date = new LocaleFormattedDate({ unixTime: Number(cachedArticle?.createdAt), locale: params?.lang });
+  const date = new LocaleFormattedDate({ unixTime: Number(cachedArticle?.createdAt), locale: currentLanguageSlug });
   const createdAtFormatted = date.getLocaleFormattedDate();
   const glossary = useSelector(selectCurrentGlossary);
 
--- src/shared/pages/Home/index.tsx
+++ src/shared/pages/Home/index.tsx
@@ -5,17 +5,17 @@ import { useLoadInitialData } from 'Hooks/useLoadInitialData';
 import { articlesLoad } from 'Modules/Articles/actions/articlesLoad';
 import { selectArticlesCurrent } from 'Modules/Articles/selectors/selectArticlesCurrent';
 import { selectCurrentGlossary } from 'Modules/Languages/selectors/selectCurrentGlossary';
-import { selectCurrentRouteParams } from 'Modules/Routes/selectors/selectCurrentRouteParams';
+import { selectCurrentLanguageSlug } from 'Modules/Languages/selectors/selectCurrentLanguageSlug';
 import { LocaleFormattedDate } from '@antoniodcorrea/utils';
 import { Home as HomeUi } from './Home';
 
 const Home: React.FC = () => {
   const dispatch = useDispatch();
   const glossary = useSelector(selectCurrentGlossary);
-  const params = useSelector(selectCurrentRouteParams);
+  const currentLanguageSlug = useSelector(selectCurrentLanguageSlug);
   const articles = useSelector(selectArticlesCurrent);
   const articlesWithDates = articles.map((item) => {
-    const date = new LocaleFormattedDate({ unixTime: Number(item?.createdAt), locale: params?.lang });
+    const date = new LocaleFormattedDate({ unixTime: Number(item?.createdAt), locale: currentLanguageSlug });
     const formattedDate = date.getLocaleFormattedDate();
 
     return {
